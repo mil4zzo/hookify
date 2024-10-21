3589775e9bbd489cd1c04c703fa41db35de2913a
mil4zzo <working.milazzo@gmail.com>
2024-10-21 12:58:04 -0300

    get_ads_details() updated to ignore empty adcreatives asset_feed_spec

--- libs/graph_api.py
+++ libs/graph_api.py
@@ -25,21 +25,21 @@ class GraphAPI:
         }
         try:
             # Debugging: Print the URL and payload
-            print('Request URL:', url)
-            print('Request Payload:', json.dumps(payload, indent=2))
+            print('get_account_info() > Request URL:', url)
+            print('get_account_info() > Request Payload:', json.dumps(payload, indent=2))
             response = requests.get(url, params=payload)
-            print('response:', response.json())
+            print('get_account_info() > response:', response.json())
             response.raise_for_status()  # Check for HTTP errors
             return {'status': 'success', 'data': response.json()}
         except requests.exceptions.HTTPError as http_err:
             decoded_url = urllib.parse.unquote(http_err.request.url) # type: ignore
             decoded_text = urllib.parse.unquote(http_err.response.text)
-            print(f'HTTP error occurred: {http_err.response.status_code} {decoded_text} for URL: {decoded_url}')  # Handle HTTP errors
+            print(f'get_account_info() > HTTP error occurred: {http_err.response.status_code} {decoded_text} for URL: {decoded_url}')  # Handle HTTP errors
             if http_err.response.json()['error']['code'] == 190:
                 return {'status': 'auth_error', 'message': decoded_text}
             return {'status': 'http_error', 'message': decoded_text}
         except Exception as err:
-            print(f'Other error occurred: {err}')  # Handle other errors
+            print(f'get_account_info() > Other error occurred: {err}')  # Handle other errors
             return {'status': 'error', 'message': str(err)}
 
     def get_page_access_token(self, actor_id):
@@ -51,11 +51,11 @@ class GraphAPI:
             for page in pages:
                 if page['id'] == actor_id:
                     self.page_token = f"?access_token={page['access_token']}"
-                    print('PAGE TOKEN:', self.page_token)
+                    print('get_page_access_token() > PAGE TOKEN:', self.page_token)
                     return self.page_token
             raise Exception(f"Page with ID {actor_id} not found")
         except requests.exceptions.RequestException as e:
-            print(f"Error getting page access token: {e}")
+            print(f"get_page_access_token() > Error getting page access token: {e}")
             return None
         
     def get_ads_details(self, act_id, time_range, ads_ids):
@@ -73,10 +73,10 @@ class GraphAPI:
 
         try:
             # Debugging: Print the URL and payload
-            req = requests.Request('GET', url, params=payload)
+            req = requests.Request('get_ads_details() > GET', url, params=payload)
             prepared = req.prepare()
             # Debugging: Print the exact URL
-            print('Request URL:', prepared.url)
+            print('get_ads_details() > Request URL:', prepared.url)
 
             insights_response = requests.get(url, params=payload)
             insights_response.raise_for_status()
@@ -87,10 +87,10 @@ class GraphAPI:
         except requests.exceptions.HTTPError as http_err:
             decoded_url = urllib.parse.unquote(http_err.request.url) # type: ignore
             decoded_text = urllib.parse.unquote(http_err.response.text)
-            print(f'HTTP error occurred: {http_err.response.status_code} {decoded_text} \n\nfor URL: {decoded_url}')  # Handle HTTP errors
+            print(f'get_ads_details() > HTTP error occurred: {http_err.response.status_code} {decoded_text} \n\nfor URL: {decoded_url}')  # Handle HTTP errors
             return None
         except Exception as err:
-            print(f'Other error occurred aqui: {err}')  # Handle other errors
+            print(f'get_ads_details() > Other error occurred aqui: {err}')  # Handle other errors
             return None
 
     def get_adaccounts(self):
@@ -100,21 +100,21 @@ class GraphAPI:
         }
         try:
             # Debugging: Print the URL and payload
-            print('Request URL:', url)
-            print('Request Payload:', json.dumps(payload, indent=2))
+            print('get_adaccounts() > Request URL:', url)
+            print('get_adaccounts() > Request Payload:', json.dumps(payload, indent=2))
             response = requests.get(url, params=payload)
-            print('response:', response)
+            print('get_adaccounts() > response:', response)
             response.raise_for_status()  # Check for HTTP errors
             return {'status': 'success', 'data': response.json()['data']}
         except requests.exceptions.HTTPError as http_err:
             decoded_url = urllib.parse.unquote(http_err.request.url) # type: ignore
             decoded_text = urllib.parse.unquote(http_err.response.text)
-            print(f'HTTP error occurred: {http_err.response.status_code} {decoded_text} for URL: {decoded_url}')  # Handle HTTP errors
+            print(f'get_adaccounts() > HTTP error occurred: {http_err.response.status_code} {decoded_text} for URL: {decoded_url}')  # Handle HTTP errors
             if http_err.response.json()['error']['code'] == 190:
                 return {'status': 'auth_error', 'message': decoded_text}
             return {'status': 'http_error', 'message': decoded_text}
         except Exception as err:
-            print(f'Other error occurred: {err}')  # Handle other errors
+            print(f'get_adaccounts() > Other error occurred: {err}')  # Handle other errors
             return {'status': 'error', 'message': str(err)}
 
     def get_ads(self, act_id, time_range, filters):
@@ -134,16 +134,16 @@ class GraphAPI:
 
         try:
             # Debugging: Print the URL and payload
-            print('Request URL:', url)
-            print('Request Payload:', json.dumps(payload, indent=2))
+            print('get_ads() > Request URL:', url)
+            print('get_ads() > Request Payload:', json.dumps(payload, indent=2))
             response = requests.post(url, params=payload)
-            print('request_url:', response.url)
+            print('get_ads() > request_url:', response.url)
             response.raise_for_status()  # Check for HTTP errors
             ad_report_id = response.json().get('report_run_id')
-            print('Current AD_REPORT_ID:', ad_report_id)
+            print('get_ads() > Current AD_REPORT_ID:', ad_report_id)
             
             if not ad_report_id:
-                print('Failed to get ad_report_id')
+                print('get_ads() > Failed to get ad_report_id')
                 return None
             
             # Polling for job completion
@@ -153,8 +153,8 @@ class GraphAPI:
                 status_response.raise_for_status()
                 status_data = status_response.json()
                 
-                print(f"{ad_report_id} STATUS", status_data['async_status'])
-                print(f"{ad_report_id} PERCENT", status_data['async_percent_completion'])
+                print(f"get_ads() > {ad_report_id} STATUS", status_data['async_status'])
+                print(f"get_ads() > {ad_report_id} PERCENT", status_data['async_percent_completion'])
 
                 loading_status = status_data['async_status']
                 loading_progress_value = status_data['async_percent_completion']
@@ -171,7 +171,7 @@ class GraphAPI:
             data = insights_response.json()['data']
 
             while 'paging' in insights_response.json() and 'next' in insights_response.json()['paging']:
-                print(f"{ad_report_id} PAGINANDO...")
+                print(f"get_ads() > {ad_report_id} PAGINANDO...")
                 insights_response = requests.get(insights_response.json()['paging']['next'])
                 insights_response.raise_for_status()
                 data.extend(insights_response.json()['data'])
@@ -194,38 +194,50 @@ class GraphAPI:
 
             # Get details for unique ads
             ads_details = self.get_ads_details(act_id, time_range, unique_ids)
+            print('got ads_details')
 
             if ads_details is not None:
+                print('ads_details is not None')
                 # Create a dictionary of ad details
                 creative_list = {detail['name']: detail['creative'] for detail in ads_details}
                 videos_list = {
                     detail['name']: detail['adcreatives']['data'][0]['asset_feed_spec']['videos']
                     for detail in ads_details
-                    if 'asset_feed_spec' in detail['adcreatives']['data'][0] and detail['adcreatives']['data'][0]['asset_feed_spec'] is not None
+                    if 'asset_feed_spec' in detail['adcreatives']['data'][0] and 'videos' in detail['adcreatives']['data'][0]['asset_feed_spec']
                 }
+
+                print(f'creative list {creative_list}')
+                print(f'video list {videos_list}')
 
                 # Update data with creative details
                 for ad in data:
+                    print(f'ad {ad_name}: start')
                     ad['creative'] = creative_list.get(ad['ad_name'], None)
                     adcreatives = videos_list.get(ad['ad_name'], None)
                     video_ids = []
                     video_thumbs = []
+                    print(f'ad {ad_name}: ad["creative"] = {ad['creative']} ')
+                    print(f'ad {ad_name}: adcreatives = {adcreatives} ')
                     if adcreatives is not None:
                         for video in adcreatives:
                             video_ids.append(video.get('video_id'))
                             video_thumbs.append(video.get('thumbnail_url'))
+                            print(f'ad {ad_name}: video_ids.append {video.get('video_id')}')
+                            print(f'ad {ad_name}: video_thumbs.append {video.get('thumbnail_url')}')
                     ad['adcreatives_videos_ids'] = video_ids
                     ad['adcreatives_videos_thumbs'] = video_thumbs
+                    print(f'ad {ad_name}: finish ad["creative"] = {ad['creative']} ')
+                    print(f'ad {ad_name}: finish adcreatives = {adcreatives} ')
 
             return data
         
         except requests.exceptions.HTTPError as http_err:
             decoded_url = urllib.parse.unquote(http_err.request.url) # type: ignore
             decoded_text = urllib.parse.unquote(http_err.response.text)
-            print(f"HTTP error occurred: {http_err.response.status_code} {decoded_text} \n\nfor URL: {decoded_url}")  # Handle HTTP errors
+            print(f"get_ads() > HTTP error occurred: {http_err.response.status_code} {decoded_text} \n\nfor URL: {decoded_url}")  # Handle HTTP errors
             return decoded_text
         except Exception as err:
-            print(f"Other error occurred: {err}")  # Handle other errors
+            print(f"get_ads() > Other error occurred: {err} or {err.args}")  # Handle other errors
             return None
 
     ## GET VIDEO SOURCE URL
@@ -249,24 +261,24 @@ class GraphAPI:
             req = requests.Request('GET', video_url, params=video_payload)
             prepared = req.prepare()
             # Debugging: Print the exact URL
-            print('Request URL:', prepared.url)
+            print('get_video_source_url() > Request URL:', prepared.url)
 
             video_response = requests.get(video_url, params=video_payload)
             video_response.raise_for_status()
             video_source = video_response.json().get('source')
 
             if video_source:
-                print('Video source:', video_source)
+                print('get_video_source_url() > Video source:', video_source)
                 return video_source
             
         except requests.exceptions.HTTPError as http_err:
             decoded_url = urllib.parse.unquote(http_err.request.url) # type: ignore
             decoded_text = urllib.parse.unquote(http_err.response.text)
-            print(f"HTTP error occurred: {http_err.response.status_code} {decoded_text} for URL: {decoded_url}")
+            print(f"get_video_source_url() > HTTP error occurred: {http_err.response.status_code} {decoded_text} for URL: {decoded_url}")
             if http_err.response.json().get('error', {}).get('code') == 190:
                 return {'status': 'auth_error', 'message': decoded_text}
             return {'status': 'http_error', 'message': decoded_text}
         
         except Exception as err:
-            print(f"Other error occurred: {err}")
+            print(f"get_video_source_url() > Other error occurred: {err}")
             return {'status': 'error', 'message': str(err)}
